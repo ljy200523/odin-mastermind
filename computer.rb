ae718ca4c11a63a3048468b11239a560340c7332
Ian Lam <ljy200523@gmail.com>
2026-05-13 14:42:07 +0800

    Fix Computer.get_computer_guess - working creator

--- computer.rb
+++ computer.rb
@@ -1,7 +1,10 @@
 class Computer
   def initialize
+    @first_guess = true
+    @guess = []
+    @next_guess = []
+    @colours = ["Blue", "Orange", "Green", "Purple", "Pink", "Brown"]
   end
-  colours = ["Blue", "Orange", "Green", "Purple", "Pink", "Brown"]
   def get_computer_answer
     colours = ["Blue", "Orange", "Green", "Purple", "Pink", "Brown"]
     @answer = []
@@ -9,34 +12,36 @@ class Computer
     return @answer
   end
   def get_computer_guess(*hint)
-    @first_guess = true
-    colours = ["Blue", "Orange", "Green", "Purple", "Pink", "Brown"]
-    @guess = []
-    @next_guess = []
-    puts hint
+    next_guess = Array.new(4, nil)
+    hint = hint.flatten
+    p "hint argument: #{hint}"
     if @first_guess
-      4.times { @guess.push(colours.sample) }
+      4.times { @guess.push(@colours.sample) }
       @first_guess = false
       return @guess #returns array
     else #2nd guess onwards
       hint.each_with_index do |element, index|
         if element == "White"
-          next_guess[index] = guess[index]
-        elsif element == "Red"
-          if next_guess.any? { |element| element == nil }
-            next_guess[element] = guess[index]
+          next_guess[index] = @guess[index]
+        end
+      end
+      nil_positions = next_guess.each_index.select { |index| next_guess[index] == nil }
+      hint.each_with_index do |element, index|
+        if element == "Red"
+          if next_guess[index].nil?
+            next_guess[nil_positions.sample] = @guess[index]
           end
-        else
-          colours.delete("#{guess[index]}")
+        elsif element == "None"
+          @colours.delete("#{@guess[index]}")
         end
       end
     end
     next_guess.each_with_index do |element, index|
       if element == nil
-        next_guess[index] = colours.sample
+        next_guess[index] = @colours.sample
       end
     end
     @guess = next_guess
-    next_guess = nil
+    return @guess
   end
 end
--- main.rb
+++ main.rb
@@ -39,7 +39,7 @@ class Mastermind
     p hint_list
     return hint_list
   end
-  def play
+  def play 
     game_mode = @player.get_game_mode
     if game_mode.downcase.strip.start_with?("g") #PLAYER IS GUESSING
       answer = @computer.get_computer_answer
@@ -64,15 +64,16 @@ class Mastermind
     elsif game_mode.downcase.strip.start_with?("c") #PLAYER IS CREATING
       answer = @player.get_player_answer
       @board.insert_answer(answer)
+      hint = nil
       12.times do |index|
-        current_guess = @computer.get_computer_guess
+        current_guess = @computer.get_computer_guess(hint)
         @board.insert_guess(current_guess)
         @board.print_board
         if @board.check_correct
           puts "Computer is correct, Computer wins"
           break
         elsif index < 11
-          get_hint(current_guess, answer)
+          hint = get_hint(current_guess, answer)
           next
         else
           puts "The Player wins"
